93fb85b3fd782b9f34601319a09e3093c13273c2
Jacob Li <jacob@jacobli.me>
2023-03-05 10:09:27 -0800

    Remove animal references

--- pages/index.js
+++ pages/index.js
@@ -3,33 +3,53 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [prompt, setPrompt] = useState("");
+  const [temperature, setTemperature] = useState(0.6);
+  const [chatHistory, setChatHistory] = useState([]);
 
   async function onSubmit(event) {
     event.preventDefault();
+    const newChatHistory = [...chatHistory, `Human: ${prompt}`];
+    setChatHistory(newChatHistory);
+
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({
+          prompt: newChatHistory.reduce(
+            (acc, currentValue) => acc + "\n" + currentValue,
+            ""
+          ),
+          temperature: temperature,
+        }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
+      setChatHistory((oldChatHistory) => [
+        ...oldChatHistory,
+        `Chatbot: ${data.result}`,
+      ]);
+      setPrompt("");
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
   }
+
+  const chatHistoryList = chatHistory.map((message) => (
+    <li key={message.toString()}>{message}</li>
+  ));
 
   return (
     <div>
@@ -39,19 +59,28 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <ul>{chatHistoryList}</ul>
+        <h3>Enter your prompt</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="prompt"
+            placeholder="Enter your prompt"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input
+            type="range"
+            name="temperature"
+            min="0"
+            max="1"
+            step="0.01"
+            defaultValue="temperature"
+            onChange={(e) => setTemperature(e.target.value)}
+          />
+          <div>{temperature}</div>
+          <input type="submit" value="Submit prompt" />
         </form>
-        <div className={styles.result}>{result}</div>
       </main>
     </div>
   );
